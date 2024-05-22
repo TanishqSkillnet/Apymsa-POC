d0abf66131611b43de0fe3c4414bbb09723c0a4a
TanishqColPal <tanishq.sharma@skillnetinc.com>
2024-05-22 22:50:20 +0530

    updated data & toggles

--- react/Apymsa.tsx
+++ react/Apymsa.tsx
@@ -1,58 +1,145 @@
-import React, { useState } from 'react'
-import { Dropdown } from 'vtex.styleguide';
-import {Button} from 'vtex.styleguide'
-import Icon from 'vtex.styleguide'
-import {IconSearch} from 'vtex.store-icons'
-import { years } from './utils/years';
-import { makes } from './utils/make';
-import { models } from './utils/model';
-interface CountdownProps {}
+import React, { useEffect, useState } from "react";
+import { Dropdown } from "vtex.styleguide";
+import { Button } from "vtex.styleguide";
+import Icon from "vtex.styleguide";
+import { IconSearch } from "vtex.store-icons";
 
+import { makes } from "./utils/make";
+import { models } from "./utils/model";
+import { vehicleData } from "./utils/schema";
+
+interface CountdownProps {}
 
 const Apymsa: StorefrontFunctionComponent<CountdownProps> = ({}) => {
 
-  const [selectedYear,setYear] = useState(1999);
-  const [selectedMake,setMake] =useState('Ford');
-  const [selectedModel,setModel] = useState('Mustang')
+  const [availableYears, setAvailableYears] = useState<any>(
+    vehicleData.map((item) => {
+      return {
+        value: `${item.year}`,
+        label: `${item.year}`,
+      };
+    })
+  );
+
+  const [selectedYear, setYear] = useState();
+
+  const [availableMakes, setMakes] = useState<any>();
+  const [selectedMake, setMake] = useState();
+
+  const [availableModels, setModels] = useState<any>();
+  const [selectedModel, setModel] = useState();
+
+  const [availableEngines,setEngines]= useState<any>();
+  const [selectedEngine,setEngine] = useState();
 
 
 
-  const handleSearch=()=>{
-      console.log('Clicked')
+
+  const handleYearChange = (v: any) => {
+    setYear(v.target.value);
+
+    const makes = vehicleData
+      .filter((item) => item.year == v.target.value)[0]
+      .makes.map((item) => {
+        return {
+          value: `${item.make}`,
+          label: `${item.make}`,
+        };
+      });
+    setMakes(makes);
+  };
+
+  const handleMakeChange = (v: any) => {
+    setMake(v.target.value);
+    const models = vehicleData
+      .filter((item) => item.year == Number(selectedYear))[0]
+      .makes.filter((item) => item.make === v.target.value)[0].models.map((item=>{
+        return {
+          value: `${item.name}`,
+          label: `${item.name}`,
+        }
+      }));
+
+    setModels(models)
+  };
+
+  const handleModelChange = (v: any) => {
+    setModel(v.target.value);
+    const engines = vehicleData
+    .filter((item) => item.year == Number(selectedYear))[0]
+    .makes.filter((item) => item.make === selectedMake)[0].models.filter(item=>item.name === v.target.value)[0].engines.map((item=>{
+      return {
+        value:`${item}`,
+        label:`${item}`
+      }
+    }));
+    console.log(engines)
+    setEngines(engines)
+  };
+
+  const handleEngineChange = (v:any)=>{
+    setEngine(v.target.value)
   }
 
+  const handleSearch = () => {
+    
+  };
 
-  return( 
+  return (
     <div className="flex flex-column   ma5 items-center justify-center ">
       <h3> Encuentra los mejores productos para tu vehículo </h3>
-      <div className='flex bg-muted-5 pa7 br4 w-100 items-center justify-between mw9 '>
+      <div className="flex bg-muted-5 pa7 br4 w-100 items-center justify-between mw9 ">
         <div className="w-90 ph3">
-          <Dropdown placeholder="Year" size="large" options={years} value ={selectedYear}/>
+          <Dropdown
+            placeholder="Year"
+            size="large"
+            options={availableYears}
+            onChange={(event: any) => handleYearChange(event)}
+            value={selectedYear}
+          />
         </div>
         <div className="w-90 ph3">
-          <Dropdown placeholder="Make" size="large" options={makes} value ={selectedMake} />
+          <Dropdown
+            placeholder="Make"
+            size="large"
+            options={availableMakes}
+            onChange={(event: any) => handleMakeChange(event)}
+            value={selectedMake}
+          />
         </div>
         <div className="w-90 ph3">
-          <Dropdown placeholder="Model" size="large" options={models} value ={selectedModel} />
+          <Dropdown
+            placeholder="Model"
+            size="large"
+            options={availableModels}
+            onChange={(event: any) => handleModelChange(event)}
+            value={selectedModel}
+          />
         </div>
         <div className="w-90 ph3">
-          <Dropdown placeholder="Enginge (Optional)" size="large"  />
+          <Dropdown 
+            placeholder="Engine (Optional)"
+            size="large" 
+            options={availableEngines}
+            onChange={(event:any)=>handleEngineChange(event)}
+            value={selectedEngine}
+             />
         </div>
         <div className="mr2 br-100 ph3 near-white ">
           <Button icon={true} variation="primary" className="br-100 ">
-            <IconSearch  />
+            <IconSearch />
           </Button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Apymsa.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {},
-}
+};
 
-export default Apymsa
+export default Apymsa;
