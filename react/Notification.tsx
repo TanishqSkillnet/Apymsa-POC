5d45b51e9dc3fbca2165bada502676bd14b0c94d
Anmol Vyas <anmol.vyas@codesquaretech.com>
2024-05-27 11:03:10 +0530

    working on notification

--- react/Notification.tsx
+++ react/Notification.tsx
@@ -2,12 +2,22 @@ import React, { useState } from 'react'
 import style from './style.css'
 
 interface Notification {
+  notification:string
 }
 
-const Notification: StorefrontFunctionComponent<Notification> = ({}) => {
+const Notification: StorefrontFunctionComponent<Notification> = ({notification}:{notification:string}) => {
   return (
     <div className={style.notification_container}>
-      
+      <div className={style.notification_image}>
+        <img src={notification} alt="" />
+      </div>
+      <div className={style.notification_content}>
+        <div className={style.ayoda_content}>
+            <div className={style.ayoda_contentDataOne}>Preguntas frecuentes</div>
+            <div className={style.ayoda_contentDataTwo}>Como comprar en Autex ?</div>
+            <div className={style.ayoda_contentDataThree}> Soporte Autex </div>
+          </div>
+        </div>
     </div>
   )
 }
@@ -17,6 +27,12 @@ Notification.schema = {
   description: 'Adress Section',
   type: 'object',
   properties: {
+    notification:{
+      title: 'Image Url',
+      description: "Image Url",
+      type: 'string',
+      default: null,
+    }
   },
 }
 
